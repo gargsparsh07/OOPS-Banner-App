1f26ee81942eb86a106d7c5e1a6c270a8677e52f
gargsparsh07 <sparshgarg.pks@gmail.com>
2026-01-31 10:46:24 +0530

    [Sparsh] Added: UC5: Render OOPS Banner using Inline Array.

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -2,15 +2,13 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        String[] banner = {
+        for (String line : new String[]{
                 " OOOOO  OOOOO  PPPPP  SSSSS ",
                 "O     O O     O P    P S     ",
                 "O     O O     O PPPPP   SSSS ",
                 "O     O O     O P           S",
                 " OOOOO  OOOOO  P       SSSSS "
-        };
-
-        for (String line : banner) {
+        }) {
             System.out.println(line);
         }
     }
