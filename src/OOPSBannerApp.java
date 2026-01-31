e9e3eb042790afd1b8e000628b7ef898a2ca7ee5
gargsparsh07 <sparshgarg.pks@gmail.com>
2026-01-31 10:51:08 +0530

    [Sparsh] Added: UC8: Use Map to Render OOPS Banner.

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,20 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class OOPSBannerApp {
 
-    static String[] OOPS = {
-            " OOOOO  OOOOO  PPPPP  SSSSS ",
-            "O     O O     O P    P S     ",
-            "O     O O     O PPPPP   SSSS ",
-            "O     O O     O P           S",
-            " OOOOO  OOOOO  P       SSSSS "
-    };
+    static Map<String, String[]> banners = new HashMap<>();
 
-    static void printBanner(String[] banner) {
-        for (String line : banner) {
+    static {
+        banners.put("OOPS", new String[]{
+                " OOOOO  OOOOO  PPPPP  SSSSS ",
+                "O     O O     O P    P S     ",
+                "O     O O     O PPPPP   SSSS ",
+                "O     O O     O P           S",
+                " OOOOO  OOOOO  P       SSSSS "
+        });
+    }
+
+    static void printBanner(String key) {
+        for (String line : banners.get(key)) {
             System.out.println(line);
         }
     }
 
     public static void main(String[] args) {
-        printBanner(OOPS);
+        printBanner("OOPS");
     }
 }
