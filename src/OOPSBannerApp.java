74da53ebd5f699237a9b925c40e743196e9345cc
gargsparsh07 <sparshgarg.pks@gmail.com>
2026-01-31 10:49:40 +0530

    [Sparsh] Added: UC7: Store OOPS Banner Pattern as Static Data.

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,5 +1,13 @@
 public class OOPSBannerApp {
 
+    static String[] OOPS = {
+            " OOOOO  OOOOO  PPPPP  SSSSS ",
+            "O     O O     O P    P S     ",
+            "O     O O     O PPPPP   SSSS ",
+            "O     O O     O P           S",
+            " OOOOO  OOOOO  P       SSSSS "
+    };
+
     static void printBanner(String[] banner) {
         for (String line : banner) {
             System.out.println(line);
@@ -7,15 +15,6 @@ public class OOPSBannerApp {
     }
 
     public static void main(String[] args) {
-
-        String[] banner = {
-                " OOOOO  OOOOO  PPPPP  SSSSS ",
-                "O     O O     O P    P S     ",
-                "O     O O     O PPPPP   SSSS ",
-                "O     O O     O P           S",
-                " OOOOO  OOOOO  P       SSSSS "
-        };
-
-        printBanner(banner);
+        printBanner(OOPS);
     }
 }
