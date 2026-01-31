e0461cf7b691b1d2d830df2cc92d6fc518b11205
gargsparsh07 <sparshgarg.pks@gmail.com>
2026-01-31 10:44:21 +0530

    [Sparsh] Added: UC4: Render OOPS Banner using Array and Loop.

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -2,14 +2,16 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        String banner = String.join("\n",
+        String[] banner = {
                 " OOOOO  OOOOO  PPPPP  SSSSS ",
                 "O     O O     O P    P S     ",
                 "O     O O     O PPPPP   SSSS ",
                 "O     O O     O P           S",
                 " OOOOO  OOOOO  P       SSSSS "
-        );
+        };
 
-        System.out.println(banner);
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
